039649a3fd4647950afdf698e7ddeb428a567b49
yixiangyin <y1027020239@qq.com>
2022-11-27 16:33:06 +1100

    clean up the source file

--- gpaCalculator.py
+++ gpaCalculator.py
@@ -1,24 +1,33 @@
 import csv
 import sys
+
+
 def main():
     if (len(sys.argv)!=2):
         print("Usage: python3 gpaCalculator.py scores.csv")
         return
     
+    scores = process(sys.argv[1])
+    get_average_score(scores)
+    get_average_gpa(scores)
+
+
+def process(filename):
     scores = {}
-    with open(sys.argv[1]) as file:
+    with open(filename) as file:
         reader = csv.DictReader(file)
         for row in reader:
             new_row = {}
             for field in row:
+                # convert to int if possible
                 if row[field].isdigit():
                     new_row[field] = int(row[field]) 
                 else:
                     new_row[field] = row[field]
             scores[row["course"]] = new_row
-    
-    get_average_score(scores)
-    get_average_gpa(scores)
+    return scores
+
+
 def get_average_gpa(scores):
     # weighted grade point score
     wgps = 0
@@ -32,6 +41,7 @@ def get_average_gpa(scores):
     gpa = round(wgps/unit, 2)
     print(f"Your GPA: {gpa}/7")
 
+
 def get_numeric_grade(score):
     if (score < 50):
         # fail
@@ -44,24 +54,32 @@ def get_numeric_grade(score):
         return 6
     else:
         return 7
+
+
 def get_grade(score):
     if (score < 50):
         # fail
         return 'F'
     elif (score < 60):
+        # pass
         return 'P'
     elif (score < 70):
+        # credit
         return 'CR'
     elif (score < 80):
+        # distinction
         return 'D'
     else:
+        # high distinction
         return 'HD'
+
+
 def get_average_score(scores):
     sum = 0
-    # without CRS
     course_count = len(scores)
     for course in scores:
-        if scores[course]["score"] != "CRS":
+        # don't take CRS into account
+        if type(scores[course]["score"]) is int: 
             sum += int(scores[course]["score"])
         else:
             course_count -= 1
